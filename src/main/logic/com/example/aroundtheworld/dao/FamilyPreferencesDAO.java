814e49d0841a771bcdf5531044bf91404a1bd79d
saramalaspina <saramalaspina2001@gmail.com>
2023-04-09 18:43:29 +0200

    delete constructor

--- src/main/logic/com/example/aroundtheworld/dao/FamilyPreferencesDAO.java
+++ src/main/logic/com/example/aroundtheworld/dao/FamilyPreferencesDAO.java
@@ -25,8 +25,6 @@ public class FamilyPreferencesDAO {
     private static final String VIDEOGAMES = "videoGames";
     private static final String COOKING = "cooking";
 
-    private FamilyPreferencesDAO(){};
-
     public static FamilyPreferences retrievePreferences(int familyId) {
         Statement stmt;
         FamilyPreferences preferences = null;
